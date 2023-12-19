28ea94d7b5cf61cd54bafc10675b085709b7f842
Alexey Lisov <lisov.alex@gmail.com>
2023-12-19 17:55:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+import typing as tp
+
+
+def parse_args(args: tp.List[str]) -> tp.Dict[str, str]:
+    for arg in args:
+        user_input = input("Please provide " + arg + ": ")
+        # TODO: Add validation of user input
+
+
+def read_commands():
+    user_input = input("Enter your command (type 'exit' to quit): ")
+
+    # Check if the user wants to exit
+    if user_input.lower() == 'exit':
+        print("Exiting the program.")
+        exit()
+    elif user_input.lower() == 'record_receipt':
+        print(parse_args(["customer_sample_name", "tube_barcode"]))
+    elif user_input.lower() == 'add_to_plate':
+        print(parse_args(["sample_id", "plate_barcode", "well_position"]))
+    elif user_input.lower() == 'tube_transfer':
+        print(parse_args(["source_tube_barcode", "destination_tube_barcode"]))
+    elif user_input.lower() == 'list_samples_in':
+        print(parse_args(["container_barcode"]))
+
+
+def main():
+    while True:
+        read_commands()
+
+
+if __name__ == "__main__":
+    main()
