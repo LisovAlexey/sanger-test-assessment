61377d2557b4a64282bb1f5ab0cb380d94776aee
Alexey Lisov <lisov.alex@gmail.com>
2023-12-22 16:43:38 +0000

    Fix add_to_plate tests (removed self) Fixed fixtures for samples Fixed errors for bad plate

--- tests/test_add_to_plate.py
+++ tests/test_add_to_plate.py
@@ -1,25 +1,21 @@
 import pytest
 from sqlalchemy import Engine
-from fixtures import database_connection, engine, database_layer
+from sqlalchemy.orm import sessionmaker
 
-from database import SampleIdBadFormatting, SampleNotFound, WellPositionBadFormatting, WellPositionOccupied, DatabaseLayer
+from fixtures import database_connection, engine, database_layer, session
+
+from database import SampleIdBadFormatting, SampleNotFound, WellPositionBadFormatting, WellPositionOccupied, \
+    DatabaseLayer, PlateBarcodeBadFormat
 from init_db import EngineCreator
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def sample_one(database_layer):
-    # transaction = database_layer.connection.begin()
-
+    # Do not need to be rolled back because sessions are rollbacked
     yield database_layer.record_receipt(customer_sample_name="test", tube_barcode="NT123")
 
-    # transaction.rollback()
-
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def sample_two(database_layer):
-    # transaction = database_layer.connection.begin()
-
     yield database_layer.record_receipt(customer_sample_name="test second sample", tube_barcode="NT333")
-
-    # transaction.rollback()
 
 class TestAddToPlate:
 
@@ -28,57 +24,57 @@ class TestAddToPlate:
 
     def test_well_position_format_number_only(self, database_layer, sample_one):
         with pytest.raises(WellPositionBadFormatting):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="1")
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="1")
 
     def test_well_position_format_char_only(self, database_layer, sample_one):
         with pytest.raises(WellPositionBadFormatting):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="A")
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="A")
 
     def test_well_position_out_of_bounds_column(self, database_layer, sample_one):
         with pytest.raises(WellPositionBadFormatting):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="A13")
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="A13")
 
     def test_well_position_out_of_bounds_row(self, database_layer, sample_one):
         with pytest.raises(WellPositionBadFormatting):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="Z1")
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="Z1")
 
     def test_well_position_out_of_bounds_mixed(self, database_layer, sample_one):
         with pytest.raises(WellPositionBadFormatting):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="Z100")
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="Z100")
 
     def test_plate_barcode_invalid_format_underscore(self, database_layer, sample_one):
-        with pytest.raises(WellPositionBadFormatting):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN_1", well_position="A1")
+        with pytest.raises(PlateBarcodeBadFormat):
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN_1", well_position="A1")
 
     def test_plate_barcode_invalid_format_missing_number(self, database_layer, sample_one):
-        with pytest.raises(WellPositionBadFormatting):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN", well_position="A1")
+        with pytest.raises(PlateBarcodeBadFormat):
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN", well_position="A1")
 
     def test_plate_barcode_invalid_format_prefix(self, database_layer, sample_one):
-        with pytest.raises(WellPositionBadFormatting):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="PR1", well_position="A1")
+        with pytest.raises(PlateBarcodeBadFormat):
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="PR1", well_position="A1")
 
     def test_plate_barcode_invalid_format_number_prefix(self, database_layer, sample_one):
-        with pytest.raises(WellPositionBadFormatting):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="1DN", well_position="A1")
+        with pytest.raises(PlateBarcodeBadFormat):
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="1DN", well_position="A1")
 
     def test_plate_adding_samples_to_several_wells(self, database_layer, sample_one):
-        database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="A1")
-        database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="A2")
-        database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="B2")
-        database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="B3")
+        database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="A1")
+        database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="A2")
+        database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="B2")
+        database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="B3")
 
     def test_well_position_occupied(self, database_layer, sample_one):
-        database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="A1")
+        database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="A1")
 
         with pytest.raises(WellPositionOccupied):
-            database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="A1")
+            database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="A1")
 
     def test_well_position_occupied_different_samples(self, database_layer, sample_one, sample_two):
-        database_layer.add_to_plate(sample_id=self.sample_one.id, plate_barcode="DN1", well_position="A1")
+        database_layer.add_to_plate(sample_id=sample_one.id, plate_barcode="DN1", well_position="A1")
 
         with pytest.raises(WellPositionOccupied):
-            database_layer.add_to_plate(sample_id=self.sample_two.id, plate_barcode="DN1", well_position="A1")
+            database_layer.add_to_plate(sample_id=sample_two.id, plate_barcode="DN1", well_position="A1")
 
     def test_well_sample_not_found(self, database_layer):
         with pytest.raises(SampleNotFound):
